724d2a621279363b90df8764703a242cf899dc1d
wishcraft <wishcraft@users.noreply.github.com>
2011-04-09 10:18:46 +0000

    XXML 1.53

---
+++ releases/1.53/htdocs/modules/xxml/language/english/admin.php
@@ -0,0 +1,12 @@
+<?php
+
+	define('_XXMLC_PLUGINAVAILABLE', 'xml Plugins Available');
+	define('_XXMLC_PLUGINAVAILABLE', 'xml Plugins Available');
+	define('_XXMLC_SELECTTABLE', 'Please Select Table to Work with');
+	define('_XXMLC_TABLESAVAILABLE', 'Tables available in database');
+	define('_XXMLC_FIELDOPTIONSFOR', 'Field options within the table selected');	
+	define('_XXMLC_VIEWSFOR', 'Views available in database');
+	define('_XXMLC_WSDLSUCCESSFUL', 'Writting WDSL Successful');
+	define('_XXMLC_WSDLUNSUCCESSFUL', 'Writting WDSL was Unsuccessful<br/>Please check the permissioning of the xxml folder and make sure it is writable');
+
+?>
